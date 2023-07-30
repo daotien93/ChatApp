50bb71cb0b77fbccec32eea2e9c49f69b22c12ae
Henrry Tien (Dao Tien) <58262966+DTien93@users.noreply.github.com>
2023-07-30 23:15:03 +0700

    Code App

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
-
+const { chats } = require('./data/data');
 const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors')
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("API is running");
 });
 
-app.get()
+// Get ALL fake data
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+})
 
-app.listen(5000, console.log('Connect to SV at port 5000'));
+// Get by Id
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find((chatId) => chatId._id === req.params.id);
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, console.log(`Server running on ${PORT}`));
